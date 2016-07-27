a389fe9f5190c5ed561a1b775b80390da7f9ae01
Draldo <draldo_lopez@hotmail.com>
2016-07-27 17:24:46 -0400

    first commit

---
+++ app/src/main/java/com/example/admin/dynamicfragments/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.admin.dynamicfragments;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        FragmentManager fragment = getSupportFragmentManager();
+        fragment.beginTransaction().add(
+                R.id.a_frame1, new BlankFragment()
+        ).commit();
+        fragment.beginTransaction().add(
+                R.id.a_frame2, new BlankFragment()
+        ).commit();
+        fragment.beginTransaction().add(
+                R.id.a_frame3, new BlankFragment()
+        ).commit();
+    }
+}
